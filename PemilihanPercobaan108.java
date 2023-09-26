4a93e272a88d2840323b300e814217e27d0856d6
claudyadestine <144087128+claudyadestine@users.noreply.github.com>
2023-09-26 13:07:54 +0700

    Add files via upload

---
+++ PemilihanPercobaan108.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+  public class PemilihanPercobaan108{
+    public static void main(String[] args){
+      Scanner input00 = new Scanner(System.in);
+
+      System.out.println("Masukkan angka: ");
+      int angka = input00.nextInt();
+
+      if (angka % 2 == 0) 
+      System.out.println("Angka "+angka+" bilangan genap");
+      else
+       System.out.println("Angka "+angka+" bilangan ganjil");
+     
+
+    }
+
+  }
